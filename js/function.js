bdff3a6472dad032a712cfa142f4c358d3fb05b3
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-04-08 18:18:48 +0200

    add: funzione di concatenazione dei numeri

---
+++ js/function.js
@@ -0,0 +1,6 @@
+function concatenaNumeri() {
+    console.log(this);
+    number+=this.innerHTML 
+    console.log(number);
+    document.getElementById("result").innerHTML = number;
+}
---
+++ js/script.js
@@ -0,0 +1,7 @@
+let num = document.querySelectorAll(".num");
+console.log (num);
+let number="";
+for (let i = 0; i < num.length; i++) {
+ num[i].addEventListener("click", concatenaNumeri);
+    
+}
